834ace35d7d69ad6de6714796a8e50a49cf7ea89
serafinon <39026710+serafinon@users.noreply.github.com>
2020-03-20 15:20:55 +0100

    init

---
+++ Assets/playerMovement.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerMovement : MonoBehaviour
+{
+    public CharacterController2D controller;
+
+    public float runSpeed = 40f;
+
+    float horizontalMove = 0f;
+
+    bool jump = false;
+ 
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalMove = Input.GetAxisRaw("Horizontal")*runSpeed;
+
+        Debug.Log(Input.GetAxisRaw("Horizontal") * runSpeed);
+        
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            Debug.Log("salto");
+            jump = true;
+        }
+    }
+
+
+    void FixedUpdate()
+    {
+        //move character
+        //controller.Move(horizontalMove, false, false);
+        controller.Move(horizontalMove*Time.fixedDeltaTime,false,jump);
+
+        jump = false;
+    }
+}
